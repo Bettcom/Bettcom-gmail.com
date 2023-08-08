1c59d8337761bf47d8d67311cf81da447660eb11
Solomon Olatunji <realsolomon01@outlook.com>
2023-08-08 23:03:55 +0100

    updates

--- app.py
+++ app.py
@@ -12,14 +12,17 @@ greeting = ""
 questions = []
 with open("question.txt", "r") as f:
     lines = f.readlines()
-    greeting = lines[0].strip()
+    greeting = lines[0].strip().split(": ")[1]
     questions = [line.strip() for line in lines[1:]]
 
-answers = {
-    "1": "Answer to question 1...",
-    "2": "Answer to question 2...",
-    "3": "Answer to question 3...",
-}
+answers = {}
+
+with open("answer.txt", "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        parts = line.strip().split(": ")
+        if len(parts) == 2:
+            answers[parts[0]] = parts[1]
 
 account_sid = os.getenv("TWILIO_ACCOUNT_SID")
 auth_token = os.getenv("TWILIO_AUTH_TOKEN")
@@ -37,13 +40,13 @@ def sendMessage(body_mess, phone_number):
 @app.route('/bot', methods=['POST'])
 def bot():
     incoming_msg = request.values['Body']
-    phone_number = (request.values['WaId'])
+    phone_number = request.values['WaId']
 
     response = MessagingResponse()
 
     if incoming_msg == "Hello" or incoming_msg == "Hi":
         all_questions = "\n".join(questions)
-        response.message(f"{greeting}\n\n{all_questions}\nPlease reply with the number of the question you want to ask.")
+        response.message(f"{greeting}\n{all_questions}")
     elif incoming_msg in [str(i + 1) for i in range(len(questions))]:
         question_index = int(incoming_msg) - 1
         response.message(answers.get(str(question_index + 1), "Sorry, I couldn't find an answer for that question."))
